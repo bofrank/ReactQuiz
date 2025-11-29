c0091496c72ca594fe5322fe1edbd44cdad1ce23
bofrank <bo@bofrank.com>
2025-11-29 22:20:28 +0900

    typos and formatting

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,9 +5,9 @@ export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
 
-    function handleSelectAnswer(selectedAnswer) {
+    function handleSelectAnswer(selectedAnswer){
         setUserAnswers((prevUserAnswers) => {
-            return [...prevUserAnswers, selectedAnswer];
+            return [...prevUserAnswers, selectedAnswer]
         });
     }
 
@@ -16,13 +16,13 @@ export default function Quiz() {
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-                        <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>
-                                {answer}
-                            </button>
-                        </li>
-                    ))}
+                    {QUESTIONS[activeQuestionIndex].answers.map(
+                        (answer) => (<li key={answer} className="answer">
+                            <button onClick={()=> handleSelectAnswer(answer)}>
+                            {answer}
+                        </button>
+                    </li>)
+                )}
                 </ul>
             </div>
         </div>
